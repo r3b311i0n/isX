c917c02e9b034db6b0d1c94e0468761d1a3c5666
r3b311i0n <nasashinega@gmail.com>
2017-11-03 16:01:02 +0530

    Do things

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 'use strict';
 
-module.exports = function isX() {
-    const X = 12;
-    return X;
-};
+const fs = require('fs');
+const path = require('path');
+
+try {
+    //  Try to parse the argument.
+    const fileName = parseInt(process.argv[2]);
+    if (isNaN(fileName)) {
+        throw new Error(`${fileName} is not a number!`);
+    } else {
+        copyTemplate(fileName.toString());
+    }
+} catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('Please enter a number!');
+}
+
+//  Make a copy of the template to modify.
+function copyTemplate(fileName) {
+    const templatePath = path.join(__dirname, './lib/template.js');
+    const destination = path.join(process.cwd(), `./${fileName}.js`);
+
+    const readStream = fs.createReadStream(templatePath);
+    readStream.once('error', (err) => {
+        // eslint-disable-next-line no-console
+        console.error(err);
+    });
+    readStream.pipe(fs.createWriteStream(destination));
+    readStream.once('end', () => transformToX(destination, fileName));
+}
+
+//  Modify the copied file.
+function transformToX(destination, fileName) {
+    let file = fs.readFileSync(destination, {
+        encoding: 'utf8'
+    });
+    file = file.replace('-9999999999999999', fileName);
+    fs.writeFileSync(destination, file, {
+        encoding: 'utf8'
+    });
+}
